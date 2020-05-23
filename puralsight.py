4e55dac2134c3d94ad8ad8a1ff192be0c89fde1f
rafitc <rafirasheed.tc99@gmail.com>
2020-05-24 04:46:24 +0530

    multiple catagory code erroe 01 fix

--- puralsight.py
+++ puralsight.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import time
 import re
 
-<<<<<<< HEAD
 #link for multiple catagory
 stack = []
 sd = "software-development"
@@ -34,20 +33,42 @@ for i in range(len(stack)):
 	request(link)
 	print("\n")
 
-=======
 
-URL = "https://www.pluralsight.com/browse/machine-learning" #link to the course
 
-def request():
+import requests 
+from bs4 import BeautifulSoup
+import time
+import re
+
+#link for multiple catagory
+stack = []
+sd = "software-development"
+cc = "cloud-computing"
+ml = "machine-learning"
+io = "it-ops"
+its = "information-cyber-security"
+dp = "data-professional"
+
+stack = [sd,cc,ml,io,its,dp]  #list for diff links
+i = 0   #loop variable for list
+
+baseURL = "https://www.pluralsight.com/browse/" #base link to the course
+
+#Req Fn start 
+def request(URL):	#Req function
 	page = requests.get(URL)
 	soup = BeautifulSoup(page.text, "html.parser")
-	url = soup.find_all('a',class_="course-item columns")
+	url = soup.find_all('a',class_="course-item columns") #course details class
 	links=[]
 	for links in url:
 		print(links.get('href')) #link collection
+#Req fn End
 
 
-request()
->>>>>>> 164f807abbd1370f6574ba42a9434e3a78a697ac
+for i in range(len(stack)):
+	link = baseURL+stack[i]
+	request(link)
+	print("\n")
+
 
 
